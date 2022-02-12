bc4c258f862f4b077c911560463d4d2331124562
Кирилл Аксенов <kirill.ax@tprofeng.ru>
2022-02-12 14:47:18 +0300

    Добавьте файлы проекта.

---
+++ Task14/Cat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task14
+{
+    class Cat : Animal
+    {
+        public string name;
+        public override string Name
+        {
+            get => name;
+            set => name=value;
+        }
+        public Cat(string name)
+            :base(name)
+        {
+           
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+}
